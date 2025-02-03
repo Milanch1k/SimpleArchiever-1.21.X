36cb0075ffc0f4e88af4c62ec346b8177d350e33
afanasev <aleksandr.afanasyev.1982@gmail.com>
2025-02-03 20:21:42 +0300

    ref: little changes to build project

--- src/main/java/net/milanchik/simplearchiever/SimpleArchiever.java
+++ src/main/java/net/milanchik/simplearchiever/SimpleArchiever.java
@@ -4,6 +4,8 @@ import com.mojang.brigadier.Command;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
@@ -19,21 +21,22 @@ public class SimpleArchiever implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            dispatcher.register(CommandManager.literal("archive").then(CommandManager.argument("pathToFile", StringArgumentType.string()).then(CommandManager.argument("pathToZIPFile", StringArgumentType.string()) // Второй аргумент
-                    .requires(source -> true).executes(context -> {
-                        String pathFile = StringArgumentType.getString(context, "pathToFile");
-                        String pathZIP = StringArgumentType.getString(context, "pathToZIPFile");
-                        ServerCommandSource source = context.getSource();
-                        try {
-                            Archiver.compressArchive(pathFile, pathZIP);
-                            source.getEntity().sendMessage(Text.translatable("successmessage"));
-                            return Command.SINGLE_SUCCESS;
-                        } catch (IOException e) {
-                            source.getEntity().sendMessage(Text.of(Text.translatable("errormessage") + e.getMessage()));
-                            return 0;
-                        }
-                    }))));
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            dispatcher.register(ClientCommandManager.literal("archive")
+                    .then(ClientCommandManager.argument("pathToFile", StringArgumentType.string())
+                            .then(ClientCommandManager.argument("pathToZIPFile", StringArgumentType.string())
+                                    .requires(source -> true).executes(context -> {
+                                        String pathFile = StringArgumentType.getString(context, "pathToFile");
+                                        String pathZIP = StringArgumentType.getString(context, "pathToZIPFile");
+                                        try {
+                                            Archiver.compressArchive(pathFile, pathZIP);
+                                            context.getSource().getEntity().sendMessage(Text.translatable("successmessage"));
+                                            return Command.SINGLE_SUCCESS;
+                                        } catch (IOException e) {
+                                            context.getSource().getEntity().sendMessage(Text.of(Text.translatable("errormessage") + e.getMessage()));
+                                            return 0;
+                                        }
+                                    }))));
         });
     }
 }
